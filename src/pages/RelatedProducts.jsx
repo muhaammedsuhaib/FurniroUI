4d033a347bf2a39e11509fc5da89d2bc0735c179
suhaib <muhammedsuhaibpottayil@gmail.com>
2024-11-23 21:18:57 +0530

    updated

--- src/pages/RelatedProducts.jsx
+++ src/pages/RelatedProducts.jsx
@@ -10,32 +10,32 @@ const products = [
     img: RelatedProducts1,
     name: "Syltherine",
     description: "Stylish cafe chair",
-    originalPrice: "Rp 3,500,000",
+    originalPrice: "Rp 3.500.000",
     salePrice: "Rp 2,500,000",
   },
   {
     id: 2,
     img: RelatedProducts2,
-    name: "Fender Guitar",
-    description: "Classic electric guitar",
-    originalPrice: "Rp 6,000,000",
-    salePrice: "Rp 4,500,000",
+    name: "Leviosa",
+    description: "Stylish cafe chair",
+    salePrice: "Rp 2.500.000",
+    percentage:-30,
   },
   {
     id: 3,
     img: RelatedProducts3,
-    name: "Marshall Speaker",
-    description: "Portable speaker",
-    originalPrice: "Rp 2,000,000",
-    salePrice: "Rp 1,500,000",
+    name: "Lolito",
+    description: "Luxury big sofa",
+    originalPrice: "Rp 14.000.000",
+    salePrice: "Rp 7.000.000",
   },
   {
     id: 4,
     img: RelatedProducts4,
-    name: "Luxury Sofa",
-    description: "Comfortable living room sofa",
-    originalPrice: "Rp 10,000,000",
-    salePrice: "Rp 8,000,000",
+    name: "Respira",
+    description: "Outdoor bar table and stool",
+    salePrice: "Rp 500.000",
+    percentage:-50,
   },
 ];
 
@@ -47,21 +47,26 @@ const RelatedProducts = () => {
       {/* Related Products Grid */}
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-4 p-12">
         {products.map((product) => (
-          <div key={product.id} className="border p-4 rounded-lg">
+          <div key={product.id} className="relative bg-[#F4F5F7]">
             <img
               src={product.img}
               alt={product.name}
-              className="w-full h-40 object-cover rounded-lg"
+              className="w-full h-48 object-cover"
             />
-            <div className="mt-4">
+            {product.percentage && (
+              <div className="absolute top-2 right-2 bg-red-500 opacity-70 text-white text-xs rounded-full p-2">
+                {product.percentage}%
+              </div>
+            )}
+            <div className="mt-4 p-5">
               <h4 className="text-lg font-semibold">{product.name}</h4>
               <p className="text-gray-600 mt-2">{product.description}</p>
               <div className="mt-2">
-                <span className="text-gray-600 line-through">
-                  {product.originalPrice}
-                </span>
-                <span className="text-lg font-semibold text-yellow-500">
+                <span className="text-lg font-semibold ">
                   {product.salePrice}
+                </span>
+                <span className="text-gray-400 text-sm line-through ml-5">
+                  {product?.originalPrice}
                 </span>
               </div>
             </div>
